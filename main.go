790f348f28624062b08f61f1c909db60cfc0e948
Kai Chen <kaichen@outlook.com>
2023-01-06 21:00:55 -0800

    Front-end code complete

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"github.com/gin-gonic/contrib/static"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.Use(static.Serve("/", static.LocalFile("./views", true)))
+
+	r.Run(":3100")
+}
